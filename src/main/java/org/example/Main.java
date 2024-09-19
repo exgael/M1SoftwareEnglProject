97a18c5d45f0dd2d804e5048d57895f0b13fa132
exgael <benoitbarbier.dev@gmail.com>
2024-09-19 21:13:43 +0200

    main

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.example.Solver.Solvers.SudokuSolution;
+import org.example.Solver.Solvers.SudokuSolver;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Sudoku Solver");
+
+        int[] easySudoku = new int[]{
+                2, 9, 0, 0, 7, 1, 0, 0, 0,
+                0, 8, 0, 3, 0, 9, 0, 0, 6,
+                0, 4, 0, 0, 0, 0, 0, 0, 0,
+                9, 0, 7, 0, 8, 0, 2, 0, 4,
+                0, 0, 0, 9, 0, 0, 6, 0, 0,
+                0, 0, 8, 0, 2, 0, 9, 1, 3,
+                0, 2, 9, 7, 0, 4, 0, 3, 8,
+                8, 0, 5, 1, 0, 0, 0, 7, 9,
+                0, 7, 4, 0, 9, 0, 1, 6, 2
+        };
+
+        SudokuSolver sudokuSolver = SudokuSolver.build();
+        SudokuSolution sol = sudokuSolver.findSudokuLevel(easySudoku);
+        System.out.println(sol);
+    }
+}
