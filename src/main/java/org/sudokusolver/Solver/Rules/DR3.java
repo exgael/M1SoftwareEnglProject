d3185b7c4ab8bf6c31c28ddca8e9625b97c83983
exgael <benoitbarbier.dev@gmail.com>
2024-09-21 14:01:09 +0200

    Use Region Class methods to simplify DR3

--- src/main/java/org/sudokusolver/Solver/Rules/DR3.java
+++ src/main/java/org/sudokusolver/Solver/Rules/DR3.java
@@ -5,6 +5,7 @@ import org.sudokusolver.Core.SudokuBoard;
 import org.sudokusolver.Core.SudokuComponents.SudokuCell;
 import org.sudokusolver.Solver.Solvers.DeductionRule;
 
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -16,29 +17,27 @@ public class DR3 implements DeductionRule {
     }
 
     private void applyNakedPair(Region region) {
-        List<SudokuCell> unsolvedCells = region.findUnsolvedCells();
-        List<SudokuCell> twoCandidatesCells = region.findCellsWithCandidateCount(2);
+        List<SudokuCell> candidateCells = region.findCellsWithCandidateCount(2);
 
-        for (int i = 0; i < twoCandidatesCells.size(); i++) {
-            var cell = twoCandidatesCells.get(i);
+        for (int i = 0; i < candidateCells.size(); i++) {
+            var cell = candidateCells.get(i);
             // Find another cell with same candidates
-            for (int j = i + 1; j < twoCandidatesCells.size(); j++) {
-                var otherCell = twoCandidatesCells.get(j);
+            for (int j = i + 1; j < candidateCells.size(); j++) {
+                var otherCell = candidateCells.get(j);
                 // Check for naked pair
-                if (cell.getCandidates().equals(otherCell.getCandidates())) {
+                if (isNakedPair(cell, otherCell)) {
                     Set<Integer> candidates = cell.getCandidates();
-                    // Remove those candidates from all the other unsolved cells
-                    unsolvedCells.forEach(unsolvedCell -> {
-                        if (!unsolvedCell.equals(cell) && !unsolvedCell.equals(otherCell)) {
-                            cell.getCandidates().forEach(unsolvedCell::removeCandidate);
-                        }
-                    });
-                    break; // This cell has already found its pair.
+                    Set<SudokuCell> pair = new HashSet<>(List.of(cell, otherCell));
+                    region.removeCandidates(candidates, pair);
+                    break; // Only one naked pair per region
                 }
             }
         }
     }
 
+    private boolean isNakedPair(SudokuCell cell1, SudokuCell cell2) {
+        return cell1.getCandidates().equals(cell2.getCandidates());
+    }
 
     private void applyNakedTriple(List<SudokuCell> threeCandidateCells, List<SudokuCell> unsolvedCells) {
         // TODO
