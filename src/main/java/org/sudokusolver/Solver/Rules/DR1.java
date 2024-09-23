e3a0f05c573ee5d60d045c0568abf777fd16e63f
laurinecd <laurinecollindufresne@gmail.com>
2024-09-23 22:03:24 +0200

    dr1 et dr2 update1

--- src/main/java/org/sudokusolver/Solver/Rules/DR1.java
+++ src/main/java/org/sudokusolver/Solver/Rules/DR1.java
@@ -1,11 +1,25 @@
 package org.sudokusolver.Solver.Rules;
 
+import org.sudokusolver.Solver.Regions.Region;
 import org.sudokusolver.Solver.Regions.RegionManager;
 import org.sudokusolver.Solver.Solvers.DeductionRule;
+import org.sudokusolver.Core.SudokuCell;
+
+import java.util.List;
+import java.util.Set;
 
 public class DR1 implements DeductionRule {
+
     @Override
     public void apply(RegionManager regionManager) {
-        // TODO
+        regionManager.forEachRegion(this::applyObviousSingle);
+    }
+
+    private void applyObviousSingle(Region region) {
+        List<SudokuCell> candidatesCells = region.findCellsWithCandidateCount(1);
+        for (SudokuCell cell : candidatesCells) {
+            Set<Integer> candidates = cell.getCandidates();
+            cell.setNumber(candidates.iterator().next());
+        }
     }
 }
--- src/main/java/org/sudokusolver/Solver/Rules/DR2.java
+++ src/main/java/org/sudokusolver/Solver/Rules/DR2.java
@@ -1,12 +1,43 @@
 package org.sudokusolver.Solver.Rules;
 
+import org.sudokusolver.Core.SudokuCell;
+import org.sudokusolver.Solver.Regions.Region;
 import org.sudokusolver.Solver.Regions.RegionManager;
 import org.sudokusolver.Solver.Solvers.DeductionRule;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class DR2 implements DeductionRule {
+
     @Override
     public void apply(RegionManager regionManager) {
-        // TODO
+        regionManager.forEachRegion(this::applyHiddenSingle);
     }
 
+    private void applyHiddenSingle(Region region) {
+        List<SudokuCell> cells = region.getCells();
+        Map<Integer, SudokuCell> candidateMap = new HashMap<>();
+
+        for (SudokuCell cell : cells) {
+            if (cell.getNumber() == 0) {
+                for (int candidate : cell.getCandidates()) {
+                    if (!candidateMap.containsKey(candidate)) {
+                        candidateMap.put(candidate, cell);
+                    } else {
+                        // if number not unique in region, we remove it (not hidden single)
+                        candidateMap.put(candidate, null);
+                    }
+                }
+            }
+        }
+
+        for (Map.Entry<Integer, SudokuCell> entry : candidateMap.entrySet()) {
+            SudokuCell cell = entry.getValue();
+            if (cell != null) {
+                cell.setNumber(entry.getKey());
+            }
+        }
+    }
 }
