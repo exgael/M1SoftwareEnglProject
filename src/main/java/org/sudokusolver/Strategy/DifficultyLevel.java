36171be4352e1c7f48179be97ee2f57e0e0708c2
exgael <benoitbarbier.dev@gmail.com>
2024-09-25 13:20:23 +0200

    Added getNext in DifficultyLevel

--- src/main/java/org/sudokusolver/Strategy/DifficultyLevel.java
+++ src/main/java/org/sudokusolver/Strategy/DifficultyLevel.java
@@ -3,6 +3,16 @@ package org.sudokusolver.Strategy;
 public enum DifficultyLevel {
     EASY,
     MEDIUM,
-    HARD,
-    EXPERT
+    HARD;
+
+    /**
+     * Get the next difficulty level
+     * <a href="https://stackoverflow.com/questions/17664445/is-there-an-increment-operator-for-java-enum">Source</a>
+     * @return the next difficulty level
+     */
+    public DifficultyLevel getNext() {
+        if (ordinal() == values().length - 1)
+            return null;
+        return values()[ordinal() + 1];
+    }
 }
