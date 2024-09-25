c17a5142a15fd8c6c10139c55e623827be7fc401
exgael <benoitbarbier.dev@gmail.com>
2024-09-25 13:01:19 +0200

    SudokuSolver now use Solver and Difficulty level. But it should be agnostic to the max level. Refactoring needed, here.

--- src/main/java/org/sudokusolver/Strategy/SudokuSolution.java
+++ src/main/java/org/sudokusolver/Strategy/SudokuSolution.java
@@ -2,5 +2,5 @@ package org.sudokusolver.Strategy;
 
 import org.sudokusolver.Gameplay.SudokuBoard;
 
-public record SudokuSolution(SudokuBoard sudokuBoard, int difficulty) {
+public record SudokuSolution(SudokuBoard sudokuBoard, DifficultyLevel difficultyLevel) {
 }
--- src/main/java/org/sudokusolver/Strategy/SudokuSolver.java
+++ src/main/java/org/sudokusolver/Strategy/SudokuSolver.java
@@ -1,54 +1,43 @@
 package org.sudokusolver.Strategy;
 
-import org.jetbrains.annotations.NotNull;
 import org.sudokusolver.Gameplay.SudokuBoard;
 import org.sudokusolver.Strategy.Regions.RegionManager;
-import org.sudokusolver.Strategy.SolvingStrategies.EasySolvingStrategy;
-import org.sudokusolver.Strategy.SolvingStrategies.HardSolvingStrategy;
-import org.sudokusolver.Strategy.SolvingStrategies.MediumSolvingStrategy;
-
-import java.util.Set;
-
 public class SudokuSolver {
-
-    private final Set<SolvingStrategy> strategies;
-
     RegionManager regionManager;
 
-    public SudokuSolver() {
-        this.strategies = Set.of(new EasySolvingStrategy(), new MediumSolvingStrategy(), new HardSolvingStrategy());
-    }
-
     public SudokuSolution findSudokuLevel(SudokuBoard sudoku) {
-        int difficulty = 0;
+        DifficultyLevel difficulty = DifficultyLevel.EASY;
         if (regionManager == null) {
             this.regionManager = new RegionManager(sudoku);
         }
-        for (var strategy : strategies) {
-            // Update sudoku level
-            difficulty++;
 
-            // Solve sudoku
-            applySolver(strategy);
-
-            // Check if sudoku is solved
+        while(difficulty != DifficultyLevel.EXPERT) {
+            Solver solver = new Solver(difficulty);
+            solver.applySolver(regionManager);
             if (sudoku.isSolved()) {
-                // Reset region manager
                 regionManager = null;
-                // if sudoku is solved, return the solution
                 return new SudokuSolution(sudoku, difficulty);
             }
+            difficulty = incrementDifficulty(difficulty);
         }
 
         regionManager = null;
-        // if sudoku is not solved, return the unsolved sudoku
-        return new SudokuSolution(sudoku, -1);
+        // return unsolved sudoku with expert level
+        return new SudokuSolution(sudoku, DifficultyLevel.EXPERT);
     }
 
-    public void applySolver(@NotNull SolvingStrategy strategy) {
-        boolean isSolving;
-        do {
-            isSolving = strategy.solve(regionManager);
-        } while (isSolving);
+    private DifficultyLevel incrementDifficulty(DifficultyLevel difficulty) {
+        switch (difficulty) {
+            case EASY -> {
+                return DifficultyLevel.MEDIUM;
+            }
+            case MEDIUM -> {
+                return DifficultyLevel.HARD;
+            }
+            case HARD -> {
+                return DifficultyLevel.EXPERT;
+            }
+            default -> throw new RuntimeException("Expert level not implemented yet");
+        }
     }
 }
