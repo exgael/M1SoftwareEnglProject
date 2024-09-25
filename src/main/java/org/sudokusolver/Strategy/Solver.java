7f40f4d3c6489ce9b323f80b1751b73d688a8b1b
exgael <benoitbarbier.dev@gmail.com>
2024-09-25 12:48:26 +0200

    Solver now use StrategyFactory to instantiate the correct strategy based on Difficulty level

--- src/main/java/org/sudokusolver/Strategy/Solver.java
+++ src/main/java/org/sudokusolver/Strategy/Solver.java
@@ -2,20 +2,18 @@ package org.sudokusolver.Strategy;
 
 import org.sudokusolver.Strategy.Regions.RegionManager;
 
-import java.util.List;
-
 public class Solver {
 
-    private final List<DeductionRule> rules;
+    private final SolvingStrategy strategy;
 
-    public Solver(List<DeductionRule> rules) {
-        this.rules = rules;
+    public Solver(DifficultyLevel level) {
+        this.strategy = StrategyFactory.createStrategy(level);
     }
 
-    public boolean solve(RegionManager regionManager) throws RuntimeException {
-        return rules.stream()
-                .map(rule -> rule.apply(regionManager))
-                .toList()
-                .contains(true);
+    public void applySolver(RegionManager regionManager) {
+        boolean isSolving;
+        do {
+            isSolving = strategy.solve(regionManager);
+        } while (isSolving);
     }
 }
