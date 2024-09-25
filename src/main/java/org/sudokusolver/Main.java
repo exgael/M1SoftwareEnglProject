a2a7fa500008ee07ab82bd14b997e045a888e5bd
exgael <benoitbarbier.dev@gmail.com>
2024-09-25 13:22:38 +0200

    corr. Main to use correct member name of SudokuSolution.

--- src/main/java/org/sudokusolver/Main.java
+++ src/main/java/org/sudokusolver/Main.java
@@ -14,25 +14,25 @@ public class Main {
         // Easy
         SudokuBoard easyBoard = new SudokuBoard(SudokuGrids.easySudoku);
         SudokuSolution easySol = sudokuSolver.findSudokuLevel(easyBoard);
-        System.out.println("Sudoku solved. Difficulty found: " + easySol.difficulty());
+        System.out.println("Sudoku solved. Difficulty found: " + easySol.difficultyLevel());
         System.out.println(easySol.sudokuBoard().debugDescription());
 
         // Medium
         SudokuBoard mediumBoard = new SudokuBoard(SudokuGrids.mediumSudoku);
         SudokuSolution mediumSol = sudokuSolver.findSudokuLevel(mediumBoard);
-        System.out.println("Sudoku solved. Difficulty found: " + mediumSol.difficulty());
+        System.out.println("Sudoku solved. Difficulty found: " + mediumSol.difficultyLevel());
         System.out.println(mediumSol.sudokuBoard().debugDescription());
 
         // Hard
         SudokuBoard hardBoard = new SudokuBoard(SudokuGrids.hardSudoku);
         SudokuSolution hardSol = sudokuSolver.findSudokuLevel(hardBoard);
-        System.out.println("Sudoku solved. Difficulty found: " + hardSol.difficulty());
+        System.out.println("Sudoku solved. Difficulty found: " + hardSol.difficultyLevel());
         System.out.println(hardSol.sudokuBoard().debugDescription());
 
         // Master
         SudokuBoard masterBoard = new SudokuBoard(SudokuGrids.masterSudoku);
         SudokuSolution masterSol = sudokuSolver.findSudokuLevel(masterBoard);
-        System.out.println("Sudoku solved. Difficulty found: " + masterSol.difficulty());
+        System.out.println("Sudoku solved. Difficulty found: " + masterSol.difficultyLevel());
         System.out.println(masterSol.sudokuBoard().debugDescription());
     }
 }
