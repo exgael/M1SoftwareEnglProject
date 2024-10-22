f364a0a94fb7ae97d6292bdf2f2e0c953d0ca26f
exgael <benoitbarbier.dev@gmail.com>
2024-10-22 18:28:32 +0200

    refactoring(GameInterface): now extends Observer<SudokuUpdate>

--- src/main/java/org/sudokusolver/Gameplay/GameInterface.java
+++ src/main/java/org/sudokusolver/Gameplay/GameInterface.java
@@ -1,6 +1,9 @@
 package org.sudokusolver.Gameplay;
 
-public interface GameInterface {
+import org.sudokusolver.Strategy.Sudoku.SudokuCellUpdate;
+import org.sudokusolver.Utils.Observer;
+
+public interface GameInterface extends Observer<SudokuCellUpdate> {
     void onRequestUserInput();
     void onSudokuFinished(int level);
     void onInvalidMove(int value, int row, int col);
