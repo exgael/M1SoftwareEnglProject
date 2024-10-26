c181aea4e5d9a321e889d6a6193ce2ad338f2dbb
laurinecd <laurinecollindufresne@gmail.com>
2024-10-26 14:42:07 +0200

    wip: updated user input

--- src/main/java/org/sudokusolver/GameInterface/SudokuGUI.java
+++ src/main/java/org/sudokusolver/GameInterface/SudokuGUI.java
@@ -2,12 +2,15 @@ package org.sudokusolver.GameInterface;
 
 import org.sudokusolver.Gameplay.GameEngine;
 import org.sudokusolver.Gameplay.GameInterface;
+import org.sudokusolver.Gameplay.UserMove;
 import org.sudokusolver.Strategy.Sudoku.SudokuUpdate;
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class SudokuGUI extends JFrame implements ActionListener, GameInterface {
 
@@ -16,6 +19,8 @@ public class SudokuGUI extends JFrame implements ActionListener, GameInterface {
     private final JButton[] numberButtons;
     private final JLabel[][] cells;
     private final GameEngine engine;
+    private int selectedValue = -1;
+
 
     public SudokuGUI(GameEngine engine) {
         super("Sudoku Solver");
@@ -34,8 +39,11 @@ public class SudokuGUI extends JFrame implements ActionListener, GameInterface {
         JPanel leftPanel = new JPanel(new BorderLayout());
         JPanel sudokuPanel = new JPanel(new GridLayout(9, 9));
         cells = new JLabel[9][9];
-        for (int row = 0; row < 9; row++) {
-            for (int col = 0; col < 9; col++) {
+        for (int Row = 0; Row < 9; Row++) {
+            for (int Col = 0; Col < 9; Col++) {
+                final int row = Row;
+                final int col = Col;
+
                 cells[row][col] = new JLabel("");
                 cells[row][col].setHorizontalAlignment(JTextField.CENTER);
 
@@ -46,6 +54,23 @@ public class SudokuGUI extends JFrame implements ActionListener, GameInterface {
                 int right = (col == 8) ? 2 : 1;
 
                 cells[row][col].setBorder(BorderFactory.createMatteBorder(top, left, bottom, right, Color.BLACK));
+
+                cells[row][col].addMouseListener(new MouseAdapter() {
+                    @Override
+                    public void mouseClicked(MouseEvent e) {
+                        if (selectedValue != -1) {
+                            cells[row][col].setText(String.valueOf(selectedValue));
+                            engine.receiveUserMove(new UserMove(row, col, selectedValue));
+                            selectedValue = -1;
+                            for (JButton btn : numberButtons) {
+                                btn.setEnabled(true);
+                            }
+                        } else {
+                            JOptionPane.showMessageDialog(null, "Please select a value first");
+                        }
+                    }
+                });
+
                 sudokuPanel.add(cells[row][col]);
             }
         }
@@ -56,12 +81,18 @@ public class SudokuGUI extends JFrame implements ActionListener, GameInterface {
         JPanel rightPanel = new JPanel(new BorderLayout());
         JPanel numberPanel = new JPanel(new GridLayout(3, 3));
         numberButtons = new JButton[9];
-        for (int i = 0; i < 9; i++) {
+        for (int I = 0; I < 9; I++) {
+            final int i = I;
             numberButtons[i] = new JButton(String.valueOf(i + 1));
-            numberPanel.add(numberButtons[i]);
+            int number = i + 1;
             numberButtons[i].addActionListener(e -> {
-                System.out.println("Clicked");
+                for (JButton btn : numberButtons) {
+                    btn.setEnabled(true);
+                }
+                selectedValue = number;
+                numberButtons[i].setEnabled(false);
             });
+            numberPanel.add(numberButtons[i]);
         }
         numberPanel.setBorder(BorderFactory.createEmptyBorder(80, 80, 80, 80));
         JPanel southPanel = new JPanel(new FlowLayout(FlowLayout.CENTER));
@@ -157,27 +188,28 @@ public class SudokuGUI extends JFrame implements ActionListener, GameInterface {
     @Override
     public void onRequestUserInput() {
         // todo: implement
-
+        //JOptionPane.showMessageDialog(this, "Le solveur ne peut pas continuer pour l'instant, veuillez entrer une valeur");
     }
 
     @Override
     public void onSudokuFinished(int difficulty) {
         // todo: implement
-        JOptionPane.showMessageDialog(this, "La difficulté est : "+levelRep(difficulty), "Fin du solveur", JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(this, "Level difficulty is : "+levelRep(difficulty), "End of solver", JOptionPane.PLAIN_MESSAGE);
     }
 
     private String levelRep(int dif) {
         return switch (dif) {
-            case 0 -> " facile :)";
-            case 1 -> " moyenne :|";
-            case 2 -> " difficile :(";
-            default -> " impossible x_x";
+            case 0 -> " Easy :)";
+            case 1 -> " Average :|";
+            case 2 -> " Hard :(";
+            default -> " Impossible ?x_x?";
         };
     }
 
     @Override
     public void onInvalidMove(int value, int row, int col) {
         // todo: implement
+        //JOptionPane.showMessageDialog(this, "Invalid move");
     }
 
     @Override
